246e74ea89d8ae12bdfbaa2f5a9c5d498762b77a
“LourencoFernando” <gastronumo@gmail.com>
2025-11-10 01:32:03 +0000

    Added comments [skip ci]

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,8 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 
+// Render the main application component into the root DOM element
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
